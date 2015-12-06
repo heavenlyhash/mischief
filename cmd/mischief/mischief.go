d2d38f3dc79c21a1578e855bf0541a5bc5236462
Eric Myhre <hash@exultant.us>
2015-12-06 16:16:50 -0600

    Draw a window.  Link glfw to do so.

--- cmd/mischief/mischief.go
+++ cmd/mischief/mischief.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"github.com/go-gl/glfw/v3.1/glfw"
+)
 
+func main() {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 2)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+	glfw.WindowHint(glfw.DepthBits, 32)
+	glfw.WindowHint(glfw.StencilBits, 0)
+
+	window, err := glfw.CreateWindow(854, 480, "Mischief", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+	glfw.SwapInterval(1) // vsync
+
+	for !window.ShouldClose() {
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
 }
