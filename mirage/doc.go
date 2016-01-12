bee7df741d25203635c376eb1b15c276810b0271
Eric Myhre <hash@exultant.us>
2016-01-12 17:58:33 -0600

    Attempt to move towards objects that self-contain their rendering routines.
    Not yet a finished abstraction.  ProgramIDs leak through and that hasn't been bundled into any useful abstraction yet.

---
+++ mirage/doc.go
@@ -0,0 +1,7 @@
+/*
+	Commute the game's logical types into their rendering engines.
+
+	(We don't let the game concrete types refer to any of the rendering
+	code or libraries because Why Would You Do That.)
+*/
+package mirage
